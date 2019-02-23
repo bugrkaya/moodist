f8f41dc8a0895e5836778eb2ec2a9485d195b015
bugra.k.wiredelta <bugra.k.wiredelta@gmail.com>
2019-02-23 21:12:03 +0300

    index is updated

--- index.js
+++ index.js
@@ -19,8 +19,11 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use((req, res, next) => {
-  res.header('Content-Type', 'application/json');
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    res.header('Content-Type', 'application/json');
+    next();
 });
 
 
