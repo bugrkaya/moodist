3e590b56a30a0986995959ecef61e74c23a76bc4
bugra.k.wiredelta <bugra.k.wiredelta@gmail.com>
2019-03-31 09:58:00 +0300

    small update

--- routers/doctor.js
+++ routers/doctor.js
@@ -1,14 +1,13 @@
 'use strict'
 
-function average(arr){
-    if (arr.length > 0 ){
+function average(arr) {
+    if (arr.length > 0) {
         var sum = 0
-        for (var i = 0; i < arr.length; i++){
+        for (var i = 0; i < arr.length; i++) {
             sum += arr[i]
         }
         return sum / arr.length
-    }
-    else return 0
+    } else return 0
 }
 
 module.exports = (app, db) => {
@@ -33,7 +32,7 @@ module.exports = (app, db) => {
                 return res.json(doctor);
             });
     });
-    
+
     // POST login
     app.post('/login', (req, res) => {
         let user = {};
@@ -50,13 +49,13 @@ module.exports = (app, db) => {
                     if (req.body.password != user.password) return res.status(401).send({
                         message: "Password is not correct!"
                     });
-                    else { 
+                    else {
                         return res.json(loggedUser);
                     }
                 }
             });
     });
-    
+
     // POST a doctor
     app.post('/doctor', (req, res) => {
         db.doctor.create({
@@ -84,42 +83,40 @@ module.exports = (app, db) => {
 
             var results = req.body.results
             var seqs = req.body.sequence
-            
+
             var right_react = []
             var wrong_react = []
-            
+
             var total_a = 0
             var total_not_a = 0
-            
+
             for (var i = 0; i < results.length; i++) {
                 if (results[i].letter === 'A') {
                     right_react.push(results[i].time)
-                }
-                else wrong_react.push(results[i].time)
+                } else wrong_react.push(results[i].time)
             }
-            
+
             for (var i = 0; i < seqs.length; i++) {
                 if (seqs[i].letter === 'A') {
                     total_a++
-                }
-                else total_not_a++
+                } else total_not_a++
             }
-            
+
             console.log('\x1b[31m%s\x1b[0m', 'right_react: ' + right_react)
             console.log('\x1b[32m%s\x1b[0m', 'wrong_react: ' + wrong_react)
             console.log('\x1b[33m%s\x1b[0m', 'total_a: ' + total_a)
             console.log('\x1b[34m%s\x1b[0m', 'total_not_a: ' + total_not_a)
             console.log('\x1b[35m%s\x1b[0m', 'avg: ' + average(right_react).toFixed(2))
-            
-            
+
+
             db.dikkat.create({
-                    dogru_basis_sayisi: right_react.length, 
-                    yanlis_basis_sayisi: wrong_react.length, 
+                    dogru_basis_sayisi: right_react.length,
+                    yanlis_basis_sayisi: wrong_react.length,
                     total_a_sayisi: total_a,
-                    total_not_a_sayisi: total_not_a, 
+                    total_not_a_sayisi: total_not_a,
                     hata_orani: 1 - (right_react.length / total_a).toFixed(3),
-                    dogrularda_tepki_suresi_ort:  average(right_react).toFixed(2),
-                    yanlislarda_tepki_suresi_ort:  average(wrong_react).toFixed(2),
+                    dogrularda_tepki_suresi_ort: average(right_react).toFixed(2),
+                    yanlislarda_tepki_suresi_ort: average(wrong_react).toFixed(2),
                     patient_id: patient_id,
                     patient_name: patient_name
                 })
@@ -129,58 +126,64 @@ module.exports = (app, db) => {
         })
     });
     
+    app.get('/durtu', (req, res) => {
+        db.durtu.findAll()
+            .then(durtus => {
+                return res.json(durtus);
+            });
+    });
+
     //POST DURTU
     app.post('/doctor/:id/patient/:patient_id/durtu', (req, res) => {
         const doctor_id = req.params.id
         const patient_id = req.params.patient_id
         var patient_name = ''
-        
+
         console.log(req.body)
-        
+
         var results = req.body.results
         var seqs = req.body.sequence
-        
+
         var right_react = []
         var wrong_react = []
 
         var total_true = 0
         var total_false = 0
-        
+
         for (var i = 0; i < results.length; i++) {
             if (results[i].color === 'green' && results[i].shape === 'circle' && results[i].key === 'n') {
                 right_react.push(results[i].time)
-            }
-            else if (results[i].color === 'red' && results[i].shape === 'polygon' && results[i].key === 'c') {
+            } else if (results[i].color === 'red' && results[i].shape === 'polygon' && results[i].key === 'c') {
                 right_react.push(results[i].time)
-            }
-            else wrong_react.push(results[i].time)
+            } else wrong_react.push(results[i].time)
         }
 
         for (var i = 0; i < seqs.length; i++) {
             if (seqs[i].color === 'green' && seqs[i].shape === 'circle' && seqs[i].key === 'n') {
                 total_true++
-            }
-            else total_false++
+            } else if (seqs[i].color === 'red' && seqs[i].shape === 'polygon' && seqs[i].key === 'c') {
+                right_react.push(results[i].time)
+            } else total_false++
         }
-        
+
         db.patient.find({
             where: {
                 id: patient_id,
                 doctor_id: doctor_id
             }
         }).then(patient => {
-            
+
             console.log('bugra')
-            
+
             patient_name = patient.dataValues.name;
             db.durtu.create({
-                    dogru_basis_sayisi: right_react.length, 
-                    yanlis_basis_sayisi: wrong_react.length, 
+                    dogru_basis_sayisi: right_react.length,
+                    yanlis_basis_sayisi: wrong_react.length,
                     total_dogru_sayisi: total_true,
                     total_yanlis_sayisi: total_false,
                     hata_orani: 1 - (right_react.length / total_true).toFixed(3),
-                    dogrularda_tepki_suresi_ort:  average(right_react).toFixed(2),
-                    yanlislarda_tepki_suresi_ort:  average(wrong_react).toFixed(2),
+                    dogrularda_tepki_suresi_ort: average(right_react).toFixed(2),
+                    yanlislarda_tepki_suresi_ort: average(wrong_react).toFixed(2),
                     patient_id: patient_id,
                     patient_name: patient_name
                 })
@@ -189,13 +192,13 @@ module.exports = (app, db) => {
                 })
         })
     });
-    
+
     //POST HAFIZA
     app.post('/doctor/:id/patient/:patient_id/hafiza', (req, res) => {
         const doctor_id = req.params.id
         const patient_id = req.params.patient_id
         var patient_name = ''
-        
+
         db.patient.find({
             where: {
                 id: patient_id,
@@ -214,7 +217,7 @@ module.exports = (app, db) => {
                 })
         })
     });
-    
+
     //POST RISK
     app.post('/doctor/:id/patient/:patient_id/risk', (req, res) => {
         const doctor_id = req.params.id
@@ -226,15 +229,15 @@ module.exports = (app, db) => {
         var total_clicks = 0
         var total_limit = 0
         var total_earining = 0
-        for (var i = 0; i < results.length; i++){
-            if (results[i].blowed == true){
+        for (var i = 0; i < results.length; i++) {
+            if (results[i].blowed == true) {
                 blown++
             }
             total_clicks += results[i].clicks
             total_limit += results[i].limit
             total_earining += results[i].earning
         }
-            
+
         db.patient.find({
             where: {
                 id: patient_id,
@@ -244,10 +247,10 @@ module.exports = (app, db) => {
             patient_name = patient.dataValues.name;
             db.risk.create({
                     ortalama_balon_kazanc: total_earining / results.length,
-                    total_kazanc : total_earining, 
+                    total_kazanc: total_earining,
                     ortalama_balon_sisirme_sayisi: (total_clicks / results.length).toFixed(2),
                     patlayan_balon_sayisi: blown,
-                    risk_alma_katsayisi : (total_clicks / total_limit).toFixed(2),
+                    risk_alma_katsayisi: (total_clicks / total_limit).toFixed(2),
                     patient_id: patient_id,
                     patient_name: patient_name
                 })
@@ -269,7 +272,7 @@ module.exports = (app, db) => {
                 return res.json(doctor);
             });
     });
-    
+
     // DELETE single patient
     app.delete('/doctor/:doctor_id/patient/:patient_id', (req, res) => {
         const id = req.params.patient_id;
@@ -283,7 +286,7 @@ module.exports = (app, db) => {
                 return res.json(doctor);
             });
     });
-    
+
     // Get all patients
     app.get('/doctor/:id/patients', (req, res) => {
         console.log('bugra')
@@ -293,7 +296,7 @@ module.exports = (app, db) => {
                 }
             })
             .then(patients => {
-               return res.json(patients);
+                return res.json(patients);
             });
     });
 
@@ -316,7 +319,7 @@ module.exports = (app, db) => {
         const name = req.body.name;
         db.doctor.find({
             where: {
-                id : req.params.id
+                id: req.params.id
             }
         }).then(doctor => (
             db.patient.create({
@@ -329,7 +332,7 @@ module.exports = (app, db) => {
             })
         ))
     });
-    
+
     // PATCH single doctor
     app.patch('/doctor/:id', (req, res) => {
         const id = req.params.id;
